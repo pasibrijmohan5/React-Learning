97741bb68ee3401595b80f6da2af207adeb1ac43
Brij <pasibrijmohan5@gmail.com>
2026-04-16 17:49:16 +0545

    Add conditional rendering

--- src/pages/Students.tsx
+++ src/pages/Students.tsx
@@ -7,6 +7,12 @@ function Students({ name, age }: StudentProps) {
         <div className="border p-3 mb-2 rounded">
             <h2>Name: {name}</h2>
             <p>Age: {age}</p>
+
+            {age >= 21 ? (
+                <p className="text-green-500">Adult</p>
+            ) : (
+                <p className="text-red-500">Minor</p>
+            )}
         </div>
     );
 }
