f702eef1809b273116a88158f19c73467b042984
Brij <pasibrijmohan5@gmail.com>
2026-04-17 17:54:16 +0545

    Show/Hide Profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import Footer from "./pages/Footer"
-import Header from "./pages/Footer"
 import Login from "./pages/Login"
 import Students from "./pages/Students"
 import ProfileCard from "./pages/ProfileCard"
+import { useState } from "react"
+
+
 
 
 const App = () => {
+  const [show, setShow] = useState(true);
   return (
     <>
       <div>
         <Login />
-
-        <header>
-          <Header />
-        </header>
         <footer>
           <Footer />
         </footer>
@@ -27,11 +26,40 @@ const App = () => {
         <ProfileCard name="Mohan" role="Designer" image="https://i.pravatar.cc/100?img=12" isOnline={false} />
       </div>
 
+      <div className="p-5">
+        <button
+          onClick={() => setShow(!show)}
+          className="bg-blue-500 text-white px-4 py-2 rounded mb-4"
+        >
+          Show / Hide ProfileCard
+        </button>
+
+
+        {show ? (
+          <div className="flex gap-4">
+            <ProfileCard
+              name="Brij"
+              role="Developer"
+              image="https://i.pravatar.cc/100?img=12"
+              isOnline={true}
+            />
+            <ProfileCard
+              name="Mohan"
+              role="Designer"
+              image="https://i.pravatar.cc/100?img=15"
+              isOnline={false}
+            />
+          </div>
+        ) : (
+          <p className="text-red-500">Profiles are hidden</p>
+        )}
+      </div>
 
 
     </>
   )
 
 }
+
 
 export default App
